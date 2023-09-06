b9e00b4c020c35fc07902a4ca22f23caa923c22f
Mariane Anjos <mariane.ferreiraanjos@outlook.com>
2023-09-05 23:58:03 -0300

    relembrando funções iniciais para fixação de conteúdo

---
+++ variaveis/script.js
@@ -0,0 +1,19 @@
+ var nome = 'Mariane';
+ console.log(nome);
+
+ var idade = '26';
+ console.log(idade);
+
+ var comidaFavorita;
+ console.log(comidaFavorita);
+
+ comidaFavorita = 'Pipoca doce'
+ console.log(comidaFavorita);
+
+ var nomePet = 'Leona,',
+    idadePet = '2 anos e 5 meses,',
+    especie = 'Cão,',
+    raça = 'estopinha,',
+    funcao = 'receber muito carinho'
+
+    console.log(nomePet,idadePet,especie,raça,funcao);
